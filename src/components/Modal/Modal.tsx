d8937b844bb6270cb83a59ef7f94e38096b712ee
Pitsyk Aleksandr <pitsyk.a@gmail.com>
2026-03-22 21:30:16 +0200

    Fix Modal.tsx - onClose for all events

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { createPortal } from 'react-dom';
 import styled from './Modal.module.css';
+import { useEffect } from 'react';
 
 interface ModalProps {
   children: React.ReactNode;
@@ -7,13 +8,45 @@ interface ModalProps {
 }
 
 export default function Modal({ children, onClose }: ModalProps) {
+  // Функція закриття модального вікна по кліку на Backdrop
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  // Еффект для перевірки натискання клавіші Esc
+  useEffect(() => {
+    // Обробник події - натискання клавіатури
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    // Додаємо слухач клавіатури на весь документ
+    document.addEventListener('keydown', handleKeyDown);
+    // Додаємо у useEffect код блокуання скролу при відкритті модалки
+    document.body.style.overflow = 'hidden';
+
+    //  При розмонтуванні компонента додаємо наступне :
+    return () => {
+      // Видалення слухача клавіатури
+      document.removeEventListener('keydown', handleKeyDown);
+      // Видаляємо з useEffect код блокування скролу
+      document.body.style.overflow = '';
+    };
+  }, [onClose]);
+
+  // Створення розмітки компонента в кінці елемента document.body за допомогою createPortal
   return createPortal(
-    <div className={styled.backdrop} role="dialog" aria-modal="true">
+    <div className={styled.backdrop} role="dialog" aria-modal="true" onClick={handleBackdropClick}>
       <div className={styled.modal}>
         <button className={styled.closeButton} aria-label="Close modal" onClick={() => onClose()}>
           &times;
         </button>
+        {/* Тут рендериться переданий вміст із пропса children */}
         {children}
+        {/* ================================================== */}
       </div>
     </div>,
     document.body
